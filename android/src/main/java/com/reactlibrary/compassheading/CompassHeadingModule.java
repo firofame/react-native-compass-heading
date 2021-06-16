426b5bce3f21ade888957a3019cd1a9ab5c2898d
David GOonzález <benaventebiker_007@hotmail.com>
2021-06-16 17:58:44 +0200

    Added rotation vector heading calculation if it is disponible

--- android/src/main/java/com/reactlibrary/compassheading/CompassHeadingModule.java
+++ android/src/main/java/com/reactlibrary/compassheading/CompassHeadingModule.java
@@ -3,7 +3,6 @@ package com.reactlibrary.compassheading;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
@@ -23,27 +22,20 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class CompassHeadingModule extends ReactContextBaseJavaModule implements SensorEventListener {
 
-    private final ReactApplicationContext reactContext;
-
     private static Context mApplicationContext;
-
-    private int mAzimuth = 0; // degree
-    private int mFilter = 1;
-
     private SensorManager sensorManager;
 
-    private Sensor gsensor;
-    private Sensor msensor;
+    private int mAzimuth = 0; // degree
+    private float mFilter = 1;
 
-    private float[] mGravity = new float[3];
-    private float[] mGeomagnetic = new float[3];
+    private final float[] mGravity = new float[3];
+    private final float[] mGeomagnetic = new float[3];
 
-    private float[] R = new float[9];
-    private float[] I = new float[9];
+    private final float[] R = new float[9];
+    private final float[] I = new float[9];
 
     public CompassHeadingModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext = reactContext;
 
         mApplicationContext = reactContext.getApplicationContext();
     }
@@ -59,11 +51,19 @@ public class CompassHeadingModule extends ReactContextBaseJavaModule implements 
         try{
             sensorManager = (SensorManager) mApplicationContext.getSystemService(Context.SENSOR_SERVICE);
 
-            gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-            msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            assert sensorManager != null;
+            Sensor gsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            Sensor msensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+            Sensor rsensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
-            sensorManager.registerListener(this, gsensor, SensorManager.SENSOR_DELAY_GAME);
-            sensorManager.registerListener(this, msensor, SensorManager.SENSOR_DELAY_GAME);
+            boolean rotationVectorExists = sensorManager.registerListener(this,
+                rsensor,
+                SensorManager.SENSOR_DELAY_GAME);
+
+            if(!rotationVectorExists){
+                sensorManager.registerListener(this, gsensor, SensorManager.SENSOR_DELAY_GAME);
+                sensorManager.registerListener(this, msensor, SensorManager.SENSOR_DELAY_GAME);
+            };
 
             mFilter = filter;
             promise.resolve(true);
@@ -86,6 +86,7 @@ public class CompassHeadingModule extends ReactContextBaseJavaModule implements 
         try{
             SensorManager manager = (SensorManager) mApplicationContext.getSystemService(Context.SENSOR_SERVICE);
 
+            assert manager != null;
             boolean res = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null &&
                 manager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
 
@@ -98,78 +99,92 @@ public class CompassHeadingModule extends ReactContextBaseJavaModule implements 
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-
+        float[] mMatrixR = new float[9];
+        float[] mMatrixValues = new float[3];
         final float alpha = 0.97f;
 
         synchronized (this) {
-            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                SensorManager.getRotationMatrixFromVector(mMatrixR, event.values);
+                SensorManager.getOrientation(mMatrixR, mMatrixValues);
+                int newAzimuth = (int) Math.toDegrees(mMatrixValues[0]);
+                newAzimuth = displayRotation((newAzimuth + 360) % 360);
+                emitResponse(newAzimuth);
+            } else {
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
-                mGravity[0] = alpha * mGravity[0] + (1 - alpha)
+                    mGravity[0] = alpha * mGravity[0] + (1 - alpha)
                         * event.values[0];
-                mGravity[1] = alpha * mGravity[1] + (1 - alpha)
+                    mGravity[1] = alpha * mGravity[1] + (1 - alpha)
                         * event.values[1];
-                mGravity[2] = alpha * mGravity[2] + (1 - alpha)
+                    mGravity[2] = alpha * mGravity[2] + (1 - alpha)
                         * event.values[2];
-            }
-
-            if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-
-                mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha)
-                        * event.values[0];
-                mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha)
-                        * event.values[1];
-                mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha)
-                        * event.values[2];
-
-            }
-
-            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
-
-            if (success) {
-
-                float orientation[] = new float[3];
-                SensorManager.getOrientation(R, orientation);
-
-                int newAzimuth = (int) Math.toDegrees(orientation[0]);
-                newAzimuth = (newAzimuth + 360) % 360;
-
-                Display disp = (((WindowManager) mApplicationContext.getSystemService(Context.WINDOW_SERVICE))).getDefaultDisplay();
-
-                if(disp != null){
-                    int rotation = disp.getRotation();
-
-                    if(rotation == Surface.ROTATION_90){
-                        newAzimuth = (newAzimuth + 90) % 360;
-                    }
-                    else if(rotation == Surface.ROTATION_270){
-                        newAzimuth = (newAzimuth + 270) % 360;
-                    }
-                    else if(rotation == Surface.ROTATION_180){
-                        newAzimuth = (newAzimuth + 180) % 360;
-                    }
                 }
 
-                if (Math.abs(mAzimuth - newAzimuth) > mFilter) {
+                if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
 
-                    mAzimuth = newAzimuth;
+                    mGeomagnetic[0] = alpha * mGeomagnetic[0] + (1 - alpha)
+                        * event.values[0];
+                    mGeomagnetic[1] = alpha * mGeomagnetic[1] + (1 - alpha)
+                        * event.values[1];
+                    mGeomagnetic[2] = alpha * mGeomagnetic[2] + (1 - alpha)
+                        * event.values[2];
 
-                    WritableMap params = Arguments.createMap();
-                    params.putDouble("heading", mAzimuth);
-                    params.putDouble("accuracy", 1.0);
+                }
 
-                    getReactApplicationContext()
-                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                        .emit("HeadingUpdated", params);
+                boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+
+                if (success) {
+
+                    float[] orientation = new float[3];
+                    SensorManager.getOrientation(R, orientation);
+
+                    int newAzimuth = (int) Math.toDegrees(orientation[0]);
+                    newAzimuth = displayRotation((newAzimuth + 360) % 360);
+                    emitResponse(newAzimuth);
                 }
             }
         }
-
-
     }
 
+    public int displayRotation (int azimuth) {
+        Display disp = (((WindowManager) mApplicationContext.getSystemService(Context.WINDOW_SERVICE))).getDefaultDisplay();
+
+        int newAzimuth = azimuth;
+
+        if(disp != null){
+            int rotation = disp.getRotation();
+
+            if(rotation == Surface.ROTATION_90){
+                newAzimuth = (azimuth + 90) % 360;
+            }
+            else if(rotation == Surface.ROTATION_270){
+                newAzimuth = (azimuth + 270) % 360;
+            }
+            else if(rotation == Surface.ROTATION_180){
+                newAzimuth = (azimuth + 180) % 360;
+            }
+        }
+
+        return newAzimuth;
+    }
+
+    public void emitResponse (int newAzimuth) {
+        if (Math.abs(mAzimuth - newAzimuth) > mFilter) {
+
+            mAzimuth = newAzimuth;
+
+            WritableMap params = Arguments.createMap();
+            params.putDouble("heading", mAzimuth);
+            params.putDouble("accuracy", 1.0);
+
+            getReactApplicationContext()
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit("HeadingUpdated", params);
+        }
+    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
--- android/src/main/java/com/reactlibrary/compassheading/CompassHeadingPackage.java
+++ android/src/main/java/com/reactlibrary/compassheading/CompassHeadingPackage.java
@@ -1,6 +1,5 @@
 package com.reactlibrary.compassheading;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,7 +12,12 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class CompassHeadingPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new CompassHeadingModule(reactContext));
+        return Collections.<NativeModule>singletonList(new CompassHeadingModule(reactContext));
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return null;
     }
 
     @Override
